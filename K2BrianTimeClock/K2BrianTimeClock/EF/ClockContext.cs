7e15c02da880f9c173ac8a5b538586a18c0b80d6
Kristopher Creel <31522425+spartandrake@users.noreply.github.com>
2018-09-11 02:43:45 -0400

    DbContext
    DbContext

---
+++ K2BrianTimeClock/K2BrianTimeClock/EF/ClockContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace K2BrianTimeClock.DAL.EF
+{
+    public class ClockContext : DbContext
+    {
+        public ClockContext()
+        {
+
+        }
+        public ClockContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            /*
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=SpyStore;Trusted_Connection=True;MultipleActiveResultSets=true;", options => options.ExecutionStrategy(c=>new MyExecutionStrategy(c)));
+            }
+            */
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=SpyStore;Trusted_Connection=True;MultipleActiveResultSets=true;", options => options.EnableRetryOnFailure());
+            }
+        }
+    }
+}
