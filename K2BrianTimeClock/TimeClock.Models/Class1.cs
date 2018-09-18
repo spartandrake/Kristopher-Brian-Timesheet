b2f758422737c95f345cb49ed425ace7ef9c20eb
Kristopher Creel <31522425+spartandrake@users.noreply.github.com>
2018-09-17 22:10:47 -0400

    Update 9/17
    Update 9/17

--- K2BrianTimeClock/TimeClock.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TimeClock.Models
-{
-    public class Class1
-    {
-    }
-}
---
+++ K2BrianTimeClock/TimeClock.Models/Entities/Base/EntityBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TimeClock.Models.Entities.Base
+{
+    public abstract class EntityBase
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Timestamp]
+        public byte[] TimeStamp { get; set; }
+    }
+}
