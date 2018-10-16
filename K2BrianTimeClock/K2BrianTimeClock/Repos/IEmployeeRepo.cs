1cff87e18d2d556d99802fd954147b05ef03bf7f
Kristopher Creel <31522425+spartandrake@users.noreply.github.com>
2018-10-15 23:43:15 -0400

    Revert "Merge branch 'master' of https://github.com/spartandrake/Kristopher-Brian-Timesheet"
    This reverts commit 78e72d9c28375ea488fb7677a05b592b189b90b3, reversing
    changes made to b1bb2d1d438ef48effa6e4afaad608753a325a37.

---
+++ K2BrianTimeClock/K2BrianTimeClock/Repos/IEmployeeRepo.cs
@@ -0,0 +1,11 @@
+﻿using K2BrianTimeClock.DAL.Repos.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace K2BrianTimeClock.DAL.Repos
+{
+    class IEmployeeRepo : IRepo<Employee>
+    {
+    }
+}
---
+++ K2BrianTimeClock/TimeClock.Models/ViewModels/EmployeeWithCurrentTimeSheet.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeClock.Models.ViewModels
+{
+    class EmployeeWithCurrentTimeSheet
+    {
+    }
+}
