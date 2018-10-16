1f06c4241ee9d5ccc621f366469a0f8491b19836
Kristopher Creel <31522425+spartandrake@users.noreply.github.com>
2018-10-15 22:26:48 -0400

    ReIRepos
    IRepos again

--- K2BrianTimeClock/K2BrianTimeClock/Repos/IClockInRepo.cs
+++ K2BrianTimeClock/K2BrianTimeClock/Repos/IClockInRepo.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using K2BrianTimeClock.DAL.Repos.Base;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace K2BrianTimeClock.DAL.Repos
 {
-    class IClockInRepo
+    class IClockInRepo : IRepo<ClockIn>
     {
     }
 }
--- K2BrianTimeClock/TimeClock.Models/ViewModels/EmployeeWithCurrentTimeSheet.cs
+++ K2BrianTimeClock/TimeClock.Models/ViewModels/EmployeeWithCurrentTimeSheet.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace TimeClock.Models.ViewModels
 {
-    class EmployeeWithTimeSheet
+    class EmployeeWithCurrentTimeSheet
     {
     }
 }
