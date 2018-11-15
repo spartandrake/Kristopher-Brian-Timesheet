b97b50dc95e005c136838eca6baa44c9ff6771d1
Tayco <Bduncan4410@gmail.com>
2018-11-15 00:19:05 -0500

    Added roles with identity

---
+++ K2BrianTimeClock/TimeClock/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TimeClock.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TimeClock.MVC.Models
+{
+    public class Employee : ApplicationUser
+    {
+
+        public string EmployeeFirstName { get; set; }
+
+   
+        public string EmployeeLastName { get; set; }
+
+        public decimal CurrentWage { get; set; }
+
+
+        public string EmailAddress { get; set; }
+
+
+        public string Password { get; set; }
+
+    }
+}
---
+++ K2BrianTimeClock/TimeClock/Models/Manager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeClock.MVC.Models
+{
+    public class Manager : Employee
+    {
+        public List<Employee> Employees { get; set; } = new List<Employee>();
+    }
+}
